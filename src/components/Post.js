c9d76b11ee69d7af91c6baaa24fdad6424dfb1b1
mohamadou moctar <moctarm99@gmail.com>
2020-11-06 14:38:33 +0600

    design of post body

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,43 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import Card from 'react-bootstrap/Card'
+import { connect } from 'react-redux'
+import { Badge, Card } from 'react-bootstrap'
+import { AiFillEdit } from 'react-icons/ai'
+import { BiDownvote, BiUpvote } from 'react-icons/bi'
 
-function Post({post}){
-    return(
-        <Card style={{ width: '18rem'}}>
-            <Card.Header>Post</Card.Header>
-            <Card.Body> 
-                <Card.Title>Post</Card.Title>
-                {post?post.title:null}
+function Post({ post }) {
+    const date=post?new Date( post.timestamp).toISOString():null
+    const postDate=post?date:null
+    console.log('date problem in post',typeof postDate)
+    return (
+        <Card style={{ width: '40rem',height: '15rem', margin: 10 }}>
+            <Card.Body>
+                <Card.Title> {post ? post.title : null}
+                    <Badge variant="success" style={{ margin: 5 }}>Edit <AiFillEdit></AiFillEdit></Badge></Card.Title>
+                <Card.Subtitle>post by jhon wick</Card.Subtitle>
+                <div>
+                    <Badge variant='primary'>Redux</Badge>
+                </div>
+                {post ? post.body : null}
+                <div style={{ marginTop: 10 }}>
+                    <BiUpvote style={{color:'green',marginRight:1}}></BiUpvote>
+                    <BiDownvote style={{color:'red'}} ></BiDownvote>
+                     <span style={{marginLeft:5,fontSize:15}}>{post ? post.voteScore : null} Votes</span>
+                </div>
+                <footer className="blockquote-footer"style={{ marginTop: 5 }} >
+                    {post ?postDate: null}
+    <p>{post ? post.commentCount : null} Comment</p>
+                </footer>
             </Card.Body>
         </Card>
     )
 }
 
 
-function mapStateToProps({posts}){
-    const postId="8xf0y6ziyjabvozdd253nd"
-    const post=posts?posts[0]:null
-    console.log('Post in post ',post)
-    return{
+function mapStateToProps({ posts }) {
+    const postId = "8xf0y6ziyjabvozdd253nd"
+    const post = posts ? posts[0] : null
+    console.log('Post in post ', post)
+    return {
         post
     }
 }
