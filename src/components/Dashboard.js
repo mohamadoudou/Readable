bf773d72361e57bfa37b1aea03f1d18a428efe57
mohamadou moctar <moctarm99@gmail.com>
2020-11-07 06:44:46 +0600

    add delete to post

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import Post from './Post'
+import '../App.css' 
 
 function Dashboard({postIds}){
     return(
-        // postIds.map((postId)=>{
-        //     return(
-        //     <li key={postId}>
-        //     <Post posts={postId}/>
-        //     </li>
-        //     )
-        // })
+
+        <div className='container'>
+            <div className='filterContainer'>
+                <p>Category</p>
+                <p>Sort by</p>
+            </div>
+        {/* postIds.map((postId)=>{
+            return(
+            <li key={postId}>
+            <Post posts={postId}/>
+            </li>
+            )
+        }) */}
         <Post />
+        </div>
     )
 }
 
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { Badge, Card } from 'react-bootstrap'
 import { AiFillEdit } from 'react-icons/ai'
 import { BiDownvote, BiUpvote } from 'react-icons/bi'
+import { MdDeleteForever } from 'react-icons/md'
 
 function Post({ post }) {
     const date=post?new Date( post.timestamp).toISOString():null
@@ -12,8 +13,10 @@ function Post({ post }) {
         <Card style={{ width: '40rem',height: '15rem', margin: 10 }}>
             <Card.Body>
                 <Card.Title> {post ? post.title : null}
-                    <Badge variant="success" style={{ margin: 5 }}>Edit <AiFillEdit></AiFillEdit></Badge></Card.Title>
-                <Card.Subtitle>post by jhon wick</Card.Subtitle>
+                    <Badge variant="success" style={{ margin: 5 }}>Edit <AiFillEdit></AiFillEdit></Badge>
+                    <Badge variant="danger" style={{ margin: 5 }}>Delete <MdDeleteForever></MdDeleteForever></Badge>
+                </Card.Title>
+    <Card.Subtitle>post by {post? post.author:null}</Card.Subtitle>
                 <div>
                     <Badge variant='primary'>Redux</Badge>
                 </div>
