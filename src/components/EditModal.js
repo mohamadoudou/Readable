056b75153e434db520af0f70b2ff105b94741cdd
mohamadou moctar <moctarm99@gmail.com>
2020-11-14 11:52:46 +0600

    add form to modal

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -1,25 +1,45 @@
 import React from 'react'
-import {Button, Modal} from 'react-bootstrap'
+import { Button, Modal, Form } from 'react-bootstrap'
 
 
 
-function EditModal(props){
+function EditModal(props) {
 
-    return(
+    return (
         <Modal
-        {...props}
-        size='lg'
-      aria-labelledby='contained-modal-title-vcenter'
-      centered
+            {...props}
+         
+            aria-labelledby='contained-modal-title-vcenter'
+            centered
         >
             <Modal.Header closeButton>
                 <Modal.Title id='contained-modal-title-vcenter'>
-                    helle Edit Modal
+                    Edit Post
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h4>Centered Modal</h4>
-                <textarea></textarea>
+                <Form>
+                    <Form.Group controlId='exampleForm.ControlInput1'>
+                        <Form.Label>Post Title</Form.Label>
+                        <Form.Control type='text' placeholder='Title' />
+                    </Form.Group>
+                    <Form.Group controlId='exampleForm.ControlTextarea1'>
+                        <Form.Label>post body</Form.Label>
+                        <Form.Control as='textarea' rows={3} />
+                    </Form.Group>
+                    <Form.Group controlId='exampleForm.ControlInput1'>
+                        <Form.Label>Post Author</Form.Label>
+                        <Form.Control type='text' placeholder='post By' />
+                    </Form.Group>
+                    <Form.Group controlId='exampleForm.ControlSelect1'>
+                        <Form.Label>Select category</Form.Label>
+                        <Form.Control as='select'>
+                            <option>react</option>
+                            <option>redux</option>
+                            <option>udacity</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick={props.onHide}>Close</Button>
@@ -28,5 +48,15 @@ function EditModal(props){
         </Modal>
     )
 }
+
+// id: post.id,
+// timestamp: post.timestamp,
+// title: post.title,
+// body: post.body,
+// author: post.author,
+// category: post.category,
+// voteScore: 1,
+// deleted: false,
+// commentCount: 0
 
 export default EditModal
