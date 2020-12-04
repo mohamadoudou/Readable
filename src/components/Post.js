4f832242b3f8dfc8b61d1fa61e79e3a2df26b8b6
mohamadou moctar <moctarm99@gmail.com>
2020-12-04 09:23:16 +0600

    redesign voting button

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,58 +8,70 @@ import PostEditModal from './PostEditModal'
 import '../App.css'
 import { deletePostData } from '../actions/post'
 
-function Post({ post,dispatch,index }) {
+function Post({ post, dispatch, index }) {
     const [modalShow, setModalShow] = useState(false)
-    const [isAddPost,setIsAddPost]=useState(true)
+    const [isEditPost, setIsEditPost] = useState(true)
     // const date=post?new Date( post.timestamp).toISOString():null
     // const postDate=post?date:null
     // console.log('date problem in post',typeof postDate)
-    const handleDelete=()=>{
-        dispatch(deletePostData(post.id,index))
-        console.log('handle delete called',post.id)
+    const handleDelete = () => {
+        dispatch(deletePostData(post.id, index))
+        console.log('handle delete called', post.id)
     }
-    return (
-        <>
-            {post&&post.deleted!==true?(<Card className='postContainer' style={{}}>
-                <Card.Body>
-                    <Card.Title> {post ? post.title : null}
-                        <button onClick={() => setModalShow(true)}>
-                            <Badge variant="success" style={{ margin: 5 }}>
-                                Edit <AiFillEdit></AiFillEdit>
-                            </Badge>
-                        </button>
-                        <button onClick={handleDelete}>
-                            <Badge variant="danger" style={{ margin: 5 }}>
-                                Delete <MdDeleteForever></MdDeleteForever>
-                            </Badge>
-                        </button>
-                    </Card.Title>
-                    <Card.Subtitle>post by {post ? post.author : null}</Card.Subtitle>
-                    <div>
-                        <Badge variant='primary'>{post?post.category:null}</Badge>
-                    </div>
-                    {post ? post.body : null}
-                    <div style={{ marginTop: 10 }}>
-                        <BiUpvote style={{ color: 'green', marginRight: 1 }}></BiUpvote>
-                        <BiDownvote style={{ color: 'red' }} ></BiDownvote>
-                        <span style={{ marginLeft: 5, fontSize: 15 }}>{post ? post.voteScore : null} Votes</span>
-                    </div>
-                    <footer className="blockquote-footer" style={{ marginTop: 5 }} >
-                        {post ? null : null}
-                        <p>{post ? post.commentCount : null} Comment</p>
-                    </footer>
-                </Card.Body>
-            </Card>):null
-            }
-            <PostEditModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-                postId={post ? post.id : null}
-                isAddPost={isAddPost}
-            />
 
-        </>
-    )
+
+    if (post && post.deleted !== true) {
+        return (
+            <>
+                <Card className='postContainer' style={{}}>
+                    <Card.Body>
+                        <Card.Title> {post ? post.title : null}
+                            <button onClick={() => setModalShow(true)}>
+                                <Badge variant="success" style={{ margin: 5 }}>
+                                    Edit <AiFillEdit></AiFillEdit>
+                                </Badge>
+                            </button>
+                            <button onClick={handleDelete}>
+                                <Badge variant="danger" style={{ margin: 5 }}>
+                                    Delete <MdDeleteForever></MdDeleteForever>
+                                </Badge>
+                            </button>
+                        </Card.Title>
+                        <Card.Subtitle>post by {post ? post.author : null}</Card.Subtitle>
+                        <div>
+                            <Badge variant='primary'>{post ? post.category : null}</Badge>
+                        </div>
+                        {post ? post.body : null}
+                        <div style={{ marginTop: 10 }}>
+                            <a>
+                                <BiUpvote style={{ color: 'green',marginBottom:-10 }} className='voteButton'></BiUpvote>
+                            </a>
+                            <div>{post ? post.voteScore : null} 
+                                <span style={{ marginLeft: 5, fontSize: 15 }}>Votes</span>
+                            </div>
+                            <a>
+                                <BiDownvote style={{ color: 'red',marginTop:-15  }} className='voteButton' ></BiDownvote>
+                            </a>
+                        </div>
+                        <footer className="blockquote-footer" style={{ marginTop: 5 }} >
+                            {post ? null : null}
+                            <p>{post ? post.commentCount : null} Comment</p>
+                        </footer>
+                    </Card.Body>
+                </Card>
+                <PostEditModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                    post={post ? post : null}
+                    isEditPost={isEditPost}
+                    index={index}
+                />
+            </>
+        )
+    }
+    else{
+        return
+    }
 }
 
 
@@ -67,7 +79,7 @@ function mapStateToProps({ posts }, { postId }) {
     const post = posts ? posts[postId] : null
     console.log('Post in post ', post)
     return {
-        index:postId,
+        index: postId,
         post
     }
 }
